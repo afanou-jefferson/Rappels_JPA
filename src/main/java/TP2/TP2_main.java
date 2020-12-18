ba7ae35befe8beb954a95eac874b9097369c36e8
Jefferon AFANOU <jefferson.afanou@gmail.com>
2020-12-18 12:20:53 +0100

    Update pour changement workspace

--- src/main/java/TP2/TP2_main.java
+++ src/main/java/TP2/TP2_main.java
@@ -1,19 +1,36 @@
 package TP2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import TP2.entite.Acteur;
+import TP2.entite.Film;
+import TP2.entite.service.ActeurService;
+import TP2.entite.service.CategorieService;
+import TP2.entite.service.FilmService;
+
 @SpringBootApplication // (exclude = { DataSourceAutoConfiguration.class })
 @EnableTransactionManagement // Annotation Spring pour autoriser la gestion des transactions
 public class TP2_main implements CommandLineRunner {
 
-	// @Autowired // Injecte le service
+	@Autowired // Injecte le service
 	// On les mets en attributs d'une classe où ils vont être appelés
-	// ActeurService acteurServ;
+	// Controller -> Service == Manger -> Repo/DAO
+	ActeurService acteurServ;
+
+	@Autowired
+	FilmService filmServ;
+
+	@Autowired
+	CategorieService categorieServ;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TP2_main.class, args);
@@ -23,47 +40,51 @@ public class TP2_main implements CommandLineRunner {
 	@Override
 	@Transactional // Démarre la transaction
 	public void run(String... args) throws Exception {
-//		// TODO Auto-generated method stub
-//
-//		// EntityManagerFactory entityManagerFactory =
-//		// Persistence.createEntityManagerFactory("films");
-//		// EntityManager entityManager = entityManagerFactory.createEntityManager();
-//
-//		// Initialisation de la transaction
-//		// EntityTransaction entityTransaction = entityManager.getTransaction();
-//		// entityTransaction.begin();
-//
-//		// Affichez les informations concernant cet acteur ainsi que les films associés
-//		// connus de
-//		// la base de données
-//		// Acteur acteurBDD = entityManager.find(Acteur.class, 1);
-//		Acteur acteurBDD = acteurServ.selectActeurByNom("WAHLBERG");
-//		System.out.println();
-//		System.out.println(acteurBDD.toString());
-//		System.out.println(acteurBDD.getStringListeFilmsDeActeur());
-//
-//		// Ajoutez un acteur : Simon Pegg et associez-le au film Mission impossible :
-//		// FALLOUT
-//		Acteur newActeur = new Acteur();
-//		newActeur.setNom("Pegg");
-//		newActeur.setPrenom("Simon");
-//		newActeur.setListeFilms(new ArrayList<Film>());
-//		acteurServ.merge(newActeur);
-//
-////		TypedQuery<Film> query = entityManager
-////				.createQuery("select f from Film f where f.titre='Mission impossible: Fallout'", Film.class);
-////		Film fallout = query.getResultList().get(0);
-////		newActeur.getListeFilms().add(fallout);
-//
-////		entityManager.persist(newActeur);
-//
-////		TypedQuery<Acteur> query2 = entityManager.createQuery("select a from Acteur a where a.prenom='Simon'",
-////				Acteur.class);
-////		Acteur simon = query2.getResultList().get(0);
-//		Acteur simon = acteurServ.selectActeurByNom("Pegg");
-//		System.out.println();
-//		System.out.println(simon.toString());
-//		System.out.println(simon.getStringListeFilmsDeActeur());
+		// TODO Auto-generated method stub
+
+		// Affichez les informations concernant cet acteur ainsi que les films associés
+		// connus de la base de données
+		Acteur acteurBDD = acteurServ.selectActeurByNom("WAHLBERG");
+		System.out.println();
+		System.out.println(acteurBDD.toString());
+		System.out.println(acteurBDD.getStringListeFilmsDeActeur());
+
+		// Ajoutez un acteur : Simon Pegg et associez-le au film Mission impossible :
+		// FALLOUT
+		Acteur newActeur = new Acteur();
+		newActeur.setNom("Pegg");
+		newActeur.setPrenom("Simon");
+		newActeur.setListeFilms(new ArrayList<Film>());
+		acteurServ.merge(newActeur);
+
+		Acteur simon = acteurServ.selectActeurByNom("Pegg");
+		System.out.println();
+		System.out.println(simon.toString());
+		System.out.println(simon.getStringListeFilmsDeActeur());
+
+		// Modifiez la date de sortie du film « Avengers : Endgame » qui est 2019 et non
+		// 2018
+		Film avengers = filmServ.selectFilmByNom("Avengers: Endgame");
+		avengers.setAnnee_sortie("2019");
+		System.out.println(avengers.toString());
+
+		// Ajoutez en base de données un film de votre choix ainsi que 3 acteurs ayant
+		// joué
+		// dans ce film
+		List<Acteur> listActeursMonFilm = new ArrayList<Acteur>();
+		listActeursMonFilm.add(simon);
+		listActeursMonFilm.add(acteurServ.selectActeurByNom("BASSETT"));
+		listActeursMonFilm.add(acteurServ.selectActeurByNom("MONAGHAN"));
+
+		Film monFilm = new Film("Drive", "2011", listActeursMonFilm, categorieServ.selectCategorieByNom("Drame"));
+
+		// Merge insert si n'existe pas alors que save(persist) va créer une nouvelle
+		// entité quoi qu'il arrive
+		filmServ.merge(monFilm);
+
+		// OK en BDD
+		categorieServ.deleteCategorieById(10);
+		categorieServ.deleteCategorieById(13);
 
 	}
 }
--- src/main/java/TP2/entite/service/CategorieService.java
+++ src/main/java/TP2/entite/service/CategorieService.java
@@ -1,6 +1,10 @@
 package TP2.entite.service;
 
+import javax.persistence.TypedQuery;
+
 import org.springframework.stereotype.Service;
+
+import TP2.entite.Categorie;
 
 @Service
 public class CategorieService extends AbstractService {
@@ -9,10 +13,32 @@ public class CategorieService extends AbstractService {
 		super();
 	}
 
-	@Override
-	public void sauvegarder(Object toSave) {
-		// TODO Auto-generated method stub
+	public Categorie selectCategorieByNom(String nomCategorieParam) {
+		TypedQuery<Categorie> query = this.em
+				.createQuery("select c from Categorie c where c.nom='" + nomCategorieParam + "'", Categorie.class);
+		Categorie categorieCherche = query.getResultList().get(0);
+		return categorieCherche;
+	}
 
+	public Categorie selectCategorieById(int idCherche) {
+		TypedQuery<Categorie> query = this.em.createQuery("select c from Categorie c where c.id='" + idCherche + "'",
+				Categorie.class);
+		Categorie categorieCherche = query.getResultList().get(0);
+		return categorieCherche;
+	}
+
+	public void deleteCategorieByName(String nomCatToDelete) {
+		Categorie toDelete = this.selectCategorieByNom(nomCatToDelete);
+		if (toDelete != null) {
+			em.remove(toDelete);
+		}
+	}
+
+	public void deleteCategorieById(int idToDelete) {
+		Categorie toDelete = this.selectCategorieById(idToDelete);
+		if (toDelete != null) {
+			em.remove(toDelete);
+		}
 	}
 
 }
